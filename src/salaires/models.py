80f8a22c0640d220f4f542525fa80085df6dcbcf
Simon Martinenq <simon.martinenq@efrei.net>
2022-03-29 22:22:10 +0200

    afficher des photo

--- src/salaires/models.py
+++ src/salaires/models.py
@@ -5,3 +5,6 @@ from django.db import models
 ## voir la vidéo à 16min 
 class Entreprise(models.Model):
     nom = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to = 'LogoEntreprise', blank=True, null=True)
+    description = models.TextField(blank=True)
+    site_web = models.CharField(max_length=250)
