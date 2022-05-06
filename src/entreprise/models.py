beabb108b4b1abe96c2b9fb9a8fefaab757f1cd5
Simon Martinenq <simon.martinenq@efrei.net>
2022-05-06 23:49:52 +0200

    creation profile utilisateur

--- src/entreprise/models.py
+++ src/entreprise/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+# voir la vidéo à 16min
+
+
+class Profile(models.Model):
+    enteprise = models.CharField(max_length=100)
+    poste = models.CharField(max_length=100)
+    salaire = models.IntegerField()
+
+    def __str__(self):
+        return self.nom
+
+    def get_absolute_url(self):
+        return reverse("entreprise_trouvee", kwargs={"slug": self.slug})
--- src/salaires/models.py
+++ src/salaires/models.py
@@ -8,8 +8,7 @@ from django.urls import reverse
 
 class Entreprise(models.Model):
     nom = models.CharField(max_length=100)
-    # identifiant nom sans espace maj etc...
-    slug = models.SlugField(max_length=128)
+    slug = models.SlugField(max_length=128)# identifiant nom sans espace maj etc...
     logo = models.ImageField(upload_to='LogoEntreprise', blank=True, null=True)
     description = models.TextField(blank=True)
     site_web = models.CharField(max_length=250)
@@ -17,5 +16,6 @@ class Entreprise(models.Model):
     def __str__(self):
         return self.nom
 
-    def get_absolute_url(self):
-        return reverse("entreprise_trouvee", kwargs={"slug": self.slug})
+    ''' def get_absolute_url(self):
+        return reverse("salaire", kwargs={"slug": self.slug})
+ '''
