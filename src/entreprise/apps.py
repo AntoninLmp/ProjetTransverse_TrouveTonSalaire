9caabaf6b811d33c712e55ed395132c3362bd0b7
Antonin Lampin <antonin.lampin@efrei.net>
2022-05-13 11:19:20 +0200

    Suppression du main

--- src/entreprise/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EntrepriseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'entreprise'
--- src/salaires/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from salaires.models import Entreprise
-
-# Register your models here.
-
-admin.site.register(Entreprise)
-    
