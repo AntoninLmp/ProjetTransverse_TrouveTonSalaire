d22efde9b598893885cbffc3224d0e4c7f40fa95
Simon Martinenq <simon.martinenq@efrei.net>
2022-05-19 23:23:22 +0200

    Ajout profile a entreprise

--- src/salaires/views.py
+++ src/salaires/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from accounts.models import utilisateur
 
 from salaires.models import Entreprise
 
@@ -8,13 +9,27 @@ from salaires.models import Entreprise
 # Create your views here.
 
 def accueil_entreprise(request):
-    entreprises = Entreprise.objects.all()
-    num = len(entreprises)
+    if request.method == "POST":
+        nom = request.POST.get("entrepriseLibelle")
+        loca = request.POST.get("localisation")
+        if nom != "" and loca != "":
+            entreprises = Entreprise.objects.filter(description__icontains=loca,nom__icontains=nom)
+        elif nom != "":
+            entreprises = Entreprise.objects.filter(nom__icontains=nom)
+        elif loca != "":
+            entreprises = Entreprise.objects.filter(description__icontains=loca)
+        else :
+            entreprises = Entreprise.objects.all()
+        num = len(entreprises)
+    else:
+        entreprises = None
+        num = 0
     return render(request, "salaires/accueil_entreprise.html", context={"entreprises" : entreprises,"nbr":num})
 
 def entreprise_trouvee(request, slug):
     entreprise = get_object_or_404(Entreprise,slug=slug)
-    return render(request, "salaires/entreprise_trouvee.html", context={"entreprise" : entreprise})
+    profiles = utilisateur.objects.filter(entreprise=entreprise.nom)
+    return render(request, "salaires/entreprise_trouvee.html", context={"entreprise" : entreprise,"profiles":profiles})
 
 def orientation(request):
     return render(request, "salaires/orientation.html")
