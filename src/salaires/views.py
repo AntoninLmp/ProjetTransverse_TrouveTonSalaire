649c736dfcd2fa7af99754e4bf52568c0db0ea62
Simon Martinenq <simon.martinenq@efrei.net>
2022-05-20 00:08:22 +0200

    comparer entreprise

--- src/salaires/views.py
+++ src/salaires/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from accounts.models import utilisateur
+from accounts.views import User
 
 from salaires.models import Entreprise
 
@@ -30,6 +31,13 @@ def entreprise_trouvee(request, slug):
     entreprise = get_object_or_404(Entreprise,slug=slug)
     profiles = utilisateur.objects.filter(entreprise=entreprise.nom)
     return render(request, "salaires/entreprise_trouvee.html", context={"entreprise" : entreprise,"profiles":profiles})
+
+def comparer_entreprise(request, slug):
+    entreprise1 = get_object_or_404(Entreprise,slug=slug)
+    slug2 = "thales"
+    entreprise2 = get_object_or_404(Entreprise,slug=slug2)
+    profiles = utilisateur.objects.filter(entreprise=entreprise1.nom)
+    return render(request, "salaires/comparer_entreprise.html", context={"entreprise1" : entreprise1,"entreprise2" : entreprise2,"profiles":profiles})
 
 def orientation(request):
     return render(request, "salaires/orientation.html")
